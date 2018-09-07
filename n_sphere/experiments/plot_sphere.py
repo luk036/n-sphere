731e99c96b960f2a25aeedc65ffd653a0082a603
Live session user <luk036@gmail.com>
2018-09-07 17:08:48 +0800

    added experiments

--- n_sphere/experiments/plot_sphere.py
+++ n_sphere/experiments/plot_sphere.py
@@ -8,13 +8,16 @@ import numpy as np
 from n_sphere.sphere import sphere
 from n_sphere.discrep_2 import discrep_2
 
+
 def average_g(triples):
     return np.mean([triple[2] for triple in triples])
+
 
 def sample_spherical(npoints, ndim=3):
     vec = np.random.randn(ndim, npoints)
     vec /= np.linalg.norm(vec, axis=0)
     return vec.transpose()
+
 
 #Triples = np.array(list(zip(X, Y, Z)))
 npoints = 600
@@ -37,9 +40,8 @@ Y = Triples[:, 1]
 Z = Triples[:, 2]
 
 collec = ax1.plot_trisurf(mtri.Triangulation(X, Y, triangles),
-        Z, shade=False, cmap=plt.get_cmap('RdYlBu'), array=colors)
+                          Z, shade=False, cmap=plt.get_cmap('RdYlBu'), array=colors)
 collec.autoscale()
-
 
 
 Triples = sample_spherical(npoints)
@@ -56,11 +58,10 @@ Y = Triples[:, 1]
 Z = Triples[:, 2]
 
 collec = ax2.plot_trisurf(mtri.Triangulation(X, Y, triangles),
-        Z, shade=False, cmap=plt.get_cmap('RdYlBu'), array=colors)
+                          Z, shade=False, cmap=plt.get_cmap('RdYlBu'), array=colors)
 collec.autoscale()
 
 plt.show()
-
 
 
 # plt.plot(points[:,0], points[:,1], 'o')
--- n_sphere/tests/test_sphere_n.py
+++ n_sphere/tests/test_sphere_n.py
@@ -6,9 +6,9 @@ from n_sphere.discrep_2 import discrep_2
 
 def test_sphere_n():
     npoints = 600
-    Triples = np.array([p for p in sphere_n(npoints, 4, [2, 3, 5, 2, 3])])
+    Triples = np.array([p for p in sphere_n(npoints, 4, [2, 3, 5, 7, 11])])
     hull = ConvexHull(Triples)
     triangles = hull.simplices
     measure = discrep_2(triangles, Triples)
-    assert measure < 1.5177
-    assert measure > 1.5176
+    assert measure < 0.913
+    assert measure > 0.912
