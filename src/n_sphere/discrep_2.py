b07df57c8d2e8c8b402f134f9111b067609f41b2
Wai-Shing Luk <luk036@gmail.com>
2019-09-07 18:53:23 +0800

    configure codecov

--- src/n_sphere/discrep_2.py
+++ src/n_sphere/discrep_2.py
@@ -16,10 +16,10 @@ def discrep_2(K, X):
     minq = 1000
     for k in range(nsimplex):
         p = X[K[k, :], :]
-        for i in range(n-1):
-            for j in range(i+1, n):
+        for i in range(n - 1):
+            for j in range(i + 1, n):
                 dot = np.dot(p[i, :], p[j, :])
-                q = 1.0 - dot*dot
+                q = 1.0 - dot * dot
                 if maxq < q:
                     maxq = q
                 if minq > q:
